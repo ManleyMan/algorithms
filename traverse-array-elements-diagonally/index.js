4385415368b3f509a0f9054b527090ff65647e15
ManleyMan <sweezymac@gmail.com>
2022-03-26 10:10:29 -0700

    solves challenge using Math.max method and return array in diagonal order

--- traverse-array-elements-diagonally/index.js
+++ traverse-array-elements-diagonally/index.js
@@ -1,24 +1,22 @@
 // Traverse array elements diagonally : "https://www.codewars.com/kata/5968fb556875980bd900000f/train/javascript"
 
 function diagonal(arr) {
-    // given a matrix (n x n array) of numbers
-    // create an empty array to return values
-    // for loop thru the main array to access the nested artraverserays
-    // nested for loop to iterate thru the sub arrays
-    // if rows + columns of index are equal then push to array
-    // 
-    // return an array with elements in diagonal order from bottom right to top left
-      
-    let result = []
-    let yLength =arr.length
-    let xLength = arr[0].length
-    
-      for( let i = 0; i < arr.length; i++){
-        let row = arr[i];
-     for ( let j=0; j < row.length; j++){
-      
-     }
+ 
+  let result = [];
+  let yLength =arr.length;
+  let xLength = arr[0].length;
+  var maxLength = Math.max(xLength,yLength);
+    for( let i = 0; i <= 2 * (maxLength - 1); i++){
+   for ( let j= yLength -1; j >= 0; j--){
+    var x = i - j
+    if(x >= 0 && x < xLength){
+      result.push(arr[j][x])
     }
+   }
+  }
+      if( result.length > 0){
+        return result.reverse()
+      }
 }
 
 
