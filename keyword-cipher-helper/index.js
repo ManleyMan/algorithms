aa87fb68344c4889ac84eaca9ef57596895865fa
ManleyMan <sweezymac@gmail.com>
2022-04-21 10:35:01 -0700

    finsihing up code

--- keyword-cipher-helper/index.js
+++ keyword-cipher-helper/index.js
@@ -8,27 +8,52 @@
 // return the cipher
 
 function KeywordCipher(abc, key) {
-    key = key.split('')
-    abc = abc.split('')
-    let newAbc = abc.filter(el => !key.includes(el))
-    let keyArr = key.concat(newAbc)
-  this.encode = function (str) {
-      let result = str.split('').map(el => {
-     if(abc.includes(el)){return abc.indexOf(el)}
-     })
+  // console.log('abc',abc)
+  // console.log('keyword',key)
   
-    let newResult = result.map(el =>  keyArr[el])
-    return newResult.join('')
-    }
-  this.decode = function (str) {
-   let answer = str.split('').map(el => {
-     if(keyArr.includes(el)){return keyArr.indexOf(el)}
-     })
+  key = key.split('')
+  abc = abc.split('')
+  let newAbc = abc.filter(letter => !key.includes(letter))
+  let keyArr = key.concat(newAbc)
+  console.log('keyArr', keyArr.length)
     
-    let newAnswer = answer.map(el =>  abc[el])
+    // encode 
+  this.encode = function (str) {
+  let strArr = str.split('')
+  console.log(strArr)
+  let result = str.split('').map(el => {
+  if(abc.includes(el)){
+    return abc.indexOf(el)
+  }else{ return el}
+  })
+  console.log('encode result', result)
+    let newResult = result.map(letter => {
+     if(keyArr[letter] && typeof letter != 'string'){
+      return keyArr[letter]
+     }else {return letter}
+    })
+  console.log('encode newResult',newResult.join(''))
+    return newResult.join('')
+  }
+    // decode
+  this.decode = function (str) {
+    let strArr = str.split('')
+    console.log('decode strArr',strArr)
+  let answer = str.split('').map(el => {
+  if(keyArr.includes(el)){
+    return keyArr.indexOf(el)
+  }else { return el}
+  })
+  console.log('decode answer', answer)
+    let newAnswer = answer.map(letter => {
+      if(abc[letter] && typeof letter != 'string'){
+        return abc[letter]
+      }else {return letter}
+    })
+  console.log('decode newAnswer',newAnswer.join(''))
     return newAnswer.join('')
-    }
-      
+  }
+  
   }
 
   // tests
